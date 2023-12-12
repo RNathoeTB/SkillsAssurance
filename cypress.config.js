a8985b3c162223a03055d56dcfee2ef107b53c71
RNathoeTB <153612084+RNathoeTB@users.noreply.github.com>
2023-12-12 22:02:02 +0100

    BaseURL

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'mihnzn',
   e2e: {
-    baseUrl: "https://testing.skillsv10.com/",
-    chromeWebSecurity: false,
+    //baseUrl: "https://testing.skillsv10.com/",
+    //chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
