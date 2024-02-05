f7ce706544d7da1322632a3654d87bb0acf08278
RNathoeTB <153612084+RNathoeTB@users.noreply.github.com>
2024-02-05 13:05:19 +0100

    Firsttestcase: Login with Username and Password

--- cypress/e2e/Core/Login & Register/Login & Logout/Login with Username and Password.cy.js
+++ cypress/e2e/Core/Login & Register/Login & Logout/Login with Username and Password.cy.js
@@ -1,13 +1,27 @@
 describe('Login with Username and Password', () => {
   
-  it.only('logs in with usernamen and password', () => {
+  it('logs in with username and password', () => {
 
-    cy.log('Observe screen')
+    cy.log('1. Observe screen')
     cy.contains('Welcome back', { timeout: 10000 }).click();
+    cy.contains('Use the username provided by your company.', { timeout: 10000 }).click();
+    cy.get('#Username')
+    cy.get('#Password')
+    cy.get('#Login')
+
+    cy.log('2. Fill in correct Username, but add an incorrect password')
     cy.get('#Username').type('Richard')
+    cy.get('#Password').type('Natho')
+   
+    cy.log('3. Click Login')
+    cy.get('#Login').click()
+    cy.contains('Wrong username or password', { timeout: 15000 }).click();
+
+    cy.log('4. Change the password by entering the valid password and click Login')
     cy.get('#Password').type('Nathoe')
     cy.get('#Login').click()
-    cy.wait(10000)
+    cy.contains('Dashboard', { timeout: 15000 }).click();
+    
   })
 
 })
