9c5a011bf99fd9c2a3ec6f3043889e7a4e63c050
RNathoeTB <153612084+RNathoeTB@users.noreply.github.com>
2024-02-05 12:06:38 +0100

    Upgrade Cypress

--- cypress/e2e/Core/Login & Register/Login & Logout/Login with Username and Password.cy.js
+++ cypress/e2e/Core/Login & Register/Login & Logout/Login with Username and Password.cy.js
@@ -2,7 +2,9 @@ describe('Login with Username and Password', () => {
   
   it.only('logs in with usernamen and password', () => {
 
-    cy.wait(10000).get('#Username').type('Richard')
+    cy.log('Observe screen')
+    cy.contains('Welcome back', { timeout: 10000 }).click();
+    cy.get('#Username').type('Richard')
     cy.get('#Password').type('Nathoe')
     cy.get('#Login').click()
     cy.wait(10000)
