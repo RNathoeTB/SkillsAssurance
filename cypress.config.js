fac99d922aa9d5fe86c695a7e60c73f741d349be
RNathoeTB <153612084+RNathoeTB@users.noreply.github.com>
2024-02-05 17:03:05 +0100

    Added testcase Forgot Password

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 15000,
+  defaultCommandTimeout: 30000,
   pageLoadTimeout: 60000,
+  env: {
+    CYPRESS_LANG: "en" // Zet de omgevingsvariabele binnen een object
+  },
   projectId: 'mihnzn',
   e2e: {
     //baseUrl: "https://testing.skillsv10.com/",
     //chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementeer hier eventuele node event listeners
     },
   },
 });
