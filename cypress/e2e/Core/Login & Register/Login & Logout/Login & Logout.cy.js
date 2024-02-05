8a676ec503e8bdbc6162b62f2c9b8d4400c16181
RNathoeTB <153612084+RNathoeTB@users.noreply.github.com>
2024-02-05 15:41:33 +0100

    Added new testcase and edited previous testcase

--- cypress/e2e/Core/Login & Register/Login & Logout/Login & Logout.cy.js
+++ cypress/e2e/Core/Login & Register/Login & Logout/Login & Logout.cy.js
@@ -4,11 +4,10 @@ describe('Login & Logout', () => {
 
     cy.log('1. Observe screen')
     cy.contains('Welcome back').click();
-    //cy.contains('Welcome back', { timeout: 10000 }).click();
     cy.contains('Use the username provided by your company.').click();
-    cy.get('#Username')
-    cy.get('#Password')
-    cy.get('#Login')
+    cy.get('#Username').should('be.visible')
+    cy.get('#Password').should('be.visible')
+    cy.get('#Login').should('be.visible')
 
     cy.log('2. Fill in correct Username, but add an incorrect password')
     cy.get('#Username').type('Richard')
@@ -16,30 +15,30 @@ describe('Login & Logout', () => {
    
     cy.log('3. Click Login')
     cy.get('#Login').click()
-    cy.contains('Wrong username or password').click();
+    cy.contains('Wrong username or password').should('be.visible')
 
     cy.log('4. Change the password by entering the valid password and click Login')
     cy.get('#Password').type('Nathoe')
     cy.get('#Login').click()
-    cy.contains('Dashboard', )
+    cy.contains('Dashboard').should('be.visible')
     
   })
 
   it('Logout', () => {
 
     cy.log('Prepare')
-    cy.contains('Welcome back').click();
-    cy.contains('Use the username provided by your company.', { timeout: 10000 }).click();
+    cy.contains('Welcome back').should('be.visible')
+    cy.contains('Use the username provided by your company.').should('be.visible')
     cy.get('#Username').type('Richard')
     cy.get('#Password').type('Nathoe')
     cy.get('#Login').click()
 
     cy.log('1. Click on the Avatar in the top part of the menu')
-    cy.contains('Dashboard')
+    cy.contains('Dashboard').should('be.visible')
     cy.get('.profile-picture').click()
     cy.log('2. Click on Logout option')
     cy.contains('Logout').click();
-    cy.contains('Welcome back').click();
+    cy.contains('Welcome back').should('be.visible')
     
   })
 
