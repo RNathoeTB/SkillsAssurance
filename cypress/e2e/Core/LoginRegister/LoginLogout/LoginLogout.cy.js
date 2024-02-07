226ba6a6f35e1237836cafde545350a964d2e681
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-02-07 17:59:23 +0100

    Testcase1328

--- cypress/e2e/Core/LoginRegister/LoginLogout/LoginLogout.cy.js
+++ cypress/e2e/Core/LoginRegister/LoginLogout/LoginLogout.cy.js
@@ -3,7 +3,7 @@ describe('Login & Logout', () => {
   it('Login with Username and Password', () => {
 
     cy.log('1. Observe screen')
-    cy.contains('Welcome back').click();
+    cy.contains('Welcome back')
     cy.contains('Use the username provided by your company.').click();
     cy.get('#Username').should('be.visible')
     cy.get('#Password').should('be.visible')
@@ -21,7 +21,7 @@ describe('Login & Logout', () => {
     cy.get('#Password').type('Nathoe')
     cy.get('#Login').click()
     cy.contains('Dashboard').should('be.visible')
-    
+    cy.get('#Login').click()
   })
 
   it('Logout', () => {
@@ -40,5 +40,29 @@ describe('Login & Logout', () => {
     cy.contains('Logout').click();
     cy.contains('Welcome back').should('be.visible')
   })
+
+  it.only('Forgot Password', () => {
+    cy.log('1. Observe opened \'Welcome back\' screen')
+    cy.contains('Forgot password?').should('be.visible')
+
+    cy.log('2. Click on \'Forgot password?\' link and observe the page')
+    cy.get('.login-card-form-bottom > :nth-child(2) > a').click()
+    cy.contains('Forgot password?').should('be.visible')
+    cy.contains('Enter your username and we\'ll send you a link to reset your password:').should('be.visible')
+    cy.get('#Username').should('have.attr', 'placeholder', 'Username')
+
+    cy.log('3. Click on the \'CANCEL\' button')
+    cy.get('#btnCancelRequestNewPassword').click()
+    cy.contains('Welcome back').should('be.visible')
+
+    cy.log('4. Repeat step 2, enter a username (that does not exist in the system')
+    cy.get('.login-card-form-bottom > :nth-child(2) > a').click()
+    cy.contains('Forgot password?').should('be.visible')
+    cy.contains('Enter your username and we\'ll send you a link to reset your password:').should('be.visible')
+    cy.get('#Username').should('have.attr', 'placeholder', 'Username')
+    cy.get('#Username').type('Ricardo')
+    cy.get('input[name="Username"]').should('have.value', 'Ricardo')
+    
+    })
 
 })
