5ce740a2b723691674a78e4c2bc14ff61f9ae34f
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-02-12 19:16:28 +0100

    nog af te maken

--- cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/Emailcategories/AddEmailCategory.cy.js
+++ cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/Emailcategories/AddEmailCategory.cy.js
@@ -14,10 +14,26 @@ it.only('Add Email Category', () => {
     cy.get('#tree-item-12 > .k-link > .k-item-text').click()
     cy.contains('Personnel settings').click()
     cy.contains('Email categories').click()
-    cy.get('.svx-grid').contains('Name').should('exist')
-    cy.get('.svx-grid').contains('Work').should('exist')
-    cy.get('.svx-grid') 
- 
+    cy.get('.svx-grid').contains('th', 'Name').should('exist')
+    cy.get('.svx-grid').contains('th', 'Name').should('exist')
+
+cy.get('k-table-td k-grid-content-sticky').find('.label').its('length').then(initialCount => {
+    
+    cy.log('2. Click on \'ADD\' button')
+    cy.get('.svx-button > .telerik-blazor').click()
+    cy.get('.svx-modal-header > div').contains('Email category').should('be.visible')
+    cy.get('.input-group > .k-textbox').should('exist')
+    cy.get(':nth-child(2) > .svx-formfield-content').should('exist')
+    cy.get('.svx-modal-buttons > :nth-child(1) > .telerik-blazor > .k-button-text').should('exist').should('have.text', 'Cancel')
+    cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').should('exist').contains('Save')
+
+    cy.log('3. Provide valid email category name and click \'CANCEL\'')
+    cy.get('.svx-modal-buttons > :nth-child(1) > .telerik-blazor').click()
+    cy.get('.svx-page-header-title').should('have.text', 'Personnel settings')
+
+cy.get('k-table-td k-grid-content-sticky').find('.label').its('length').should('eq', initialCount + 1);
+        })
+
 
 
 
