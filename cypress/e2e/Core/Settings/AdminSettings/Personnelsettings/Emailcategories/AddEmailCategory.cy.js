ea77ec401844c9e70cbb004523177ae413001a1f
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-02-13 15:22:32 +0100

    not finished

--- cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/Emailcategories/AddEmailCategory.cy.js
+++ cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/Emailcategories/AddEmailCategory.cy.js
@@ -1,4 +1,4 @@
-describe('Requirements', () => {
+describe('Settings', () => {
   beforeEach(() => {
     // Voeg hier de stappen toe die je voor elke test wilt uitvoeren
     cy.log('Login')
@@ -17,7 +17,6 @@ it.only('Add Email Category', () => {
     cy.get('.svx-grid').contains('th', 'Name').should('exist')
     cy.get('.svx-grid').contains('th', 'Name').should('exist')
 
-    cy.get('k-table-td k-grid-content-sticky').find('.label').its('length').then(initialCount => {
     
     cy.log('2. Click on \'ADD\' button')
     cy.get('.svx-button > .telerik-blazor').click()
@@ -26,13 +25,39 @@ it.only('Add Email Category', () => {
     cy.get(':nth-child(2) > .svx-formfield-content').should('exist')
     cy.get('.svx-modal-buttons > :nth-child(1) > .telerik-blazor > .k-button-text').should('exist').should('have.text', 'Cancel')
     cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').should('exist').contains('Save')
-
+    
     cy.log('3. Provide valid email category name and click \'CANCEL\'')
+    cy.get('.input-group > .k-textbox').type('TEST')
+    // Click Cancel
     cy.get('.svx-modal-buttons > :nth-child(1) > .telerik-blazor').click()
-    cy.get('.svx-page-header-title').should('have.text', 'Personnel settings')
 
-    cy.get('k-table-td k-grid-content-sticky').find('.label').its('length').should('eq', initialCount + 1);
-        })
+    // Assertion 1: Verify the pop-up dialog is closed
+    cy.get('.svx-modal-header > div').should('not.exist')
+
+    // Assertion 2: Verify the user is redirected back to the email categories overview screen
+    cy.get('[data-text="Name"] > .k-cell-inner > .k-link').should('exist')
+    cy.get('[data-text="Work"] > .k-cell-inner > .k-link').should('exist')
+    cy.get('.svx-grid-footer-template').should('exist')
+
+    // Assertion 3: Verify new email category is not created
+    cy.get('.k-grid-content').contains('TEST').should('not.exist')
+
+    cy.log('4. Click \'ADD\', provide valid name "WorkART" and click \'SAVE\' button')
+    cy.get('.svx-button > .telerik-blazor').click()
+    cy.get('.input-group > .k-textbox').type('WorkART')
+    cy.wait(1000)
+    cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').click()
+    // Check if the notification containing "Save successful" text is visible
+    cy.get('.k-notification-content').contains('Save successful').should('be.visible')
+
+    cy.get('#tree-item-12 > .k-link > .k-item-text').click()
+    cy.contains('Personnel settings').click()
+    cy.contains('Email categories').click()
+    cy.get('.k-grid-content').contains('WorkART').should('.exist')
+
+ 
+    
+
 
 
 
