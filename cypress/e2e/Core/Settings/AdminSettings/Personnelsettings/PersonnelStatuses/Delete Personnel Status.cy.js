eecdd88943411545130f15518e08dbdc736285a6
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-02-23 14:21:02 +0100

    TC 924 Delete Personnel Status, FineTuned, Finished

--- cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/PersonnelStatuses/Delete Personnel Status.cy.js
+++ cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/PersonnelStatuses/Delete Personnel Status.cy.js
@@ -6,7 +6,9 @@ describe('Settings > Personnel settings', () => {
      cy.get('#Username').type('Richard')
      cy.get('#Password').type('Nathoe')
      cy.get('#Login').click()
-
+     // Test data:
+     // 'Status 1' - is defined at list for 1 employee
+     // 'Status 2' - is created but not used yet
      cy.get('#tree-item-12 > .k-link > .k-item-text').click()
      cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
      cy.get('.k-tabstrip-items').contains('Personnel statuses').click()
@@ -19,14 +21,13 @@ describe('Settings > Personnel settings', () => {
      cy.get(':nth-child(1) > .svx-formfield-content > .input-group > .k-textbox').clear().type('Status 2')
      cy.wait(1000)
      cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').click()
-
      //  Navigate to Employees overview grid
      cy.get('#tree-item-4 > .k-link > .telerik-blazor').click()
      cy.get('#tree-item-4_0 > .k-link > .k-item-text') .click()
      //  open an Employee profile (with already a status)
-     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('Ritchie Nathoe ()')
-     cy.wait(4000)
-     cy.get('.k-grid-content').should('contain', 'Ritchie Nathoe').contains('Ritchie Nathoe ()').click()
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('Ritchie Nathoe')
+     cy.wait(2000)
+     cy.get('.k-grid-content').contains('Ritchie Nathoe').click()
      //  within the Organization tab, click \'EDIT\' and open the Personnel status drop down
      cy.wait(3000)
      cy.get('.k-tabstrip-item:contains("Organization")').click()
@@ -36,13 +37,9 @@ describe('Settings > Personnel settings', () => {
      cy.get('.svx-column-block-right > .svx-block > .svx-block-body > :nth-child(1) > .svx-formfield-content').find('input.k-input-inner').clear().type('Status 1').wait(3000).type('{enter}')
      cy.get('.svx-column-block-left > :nth-child(1) > .svx-block-body > :nth-child(4) > .svx-formfield-content').find('input.k-input-inner').clear().type('NM Organisational Unit Europe').wait(3000).type('{enter}')
      cy.get('.modal-buttons > :nth-child(2) > .telerik-blazor').click()
-    
      });
-  
-    // Test data:
-    // 'Status 1' - is defined at list for 1 employee
-    // 'Status 2' - is created but not used yet
-    it.only('Validation on Add/Edit Personnel Status', () => {
+
+    it('Validation on Add/Edit Personnel Status', () => {
      cy.log('1. Click bin icon to \'Status 1\' record')
      cy.get('#tree-item-12 > .k-link > .k-item-text').click()
      cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
@@ -62,7 +59,6 @@ describe('Settings > Personnel settings', () => {
     // The validation pop-up dialog is opened with the following message:Delete failed because the item is used by: Ritchie Nathoe
     // cy.contains(' cy.contains(' Are you sure you want to delete the selected item?').should('exist')').should('exist')
     // 'CLOSE' button is present
-
 
      cy.log('3. Click \'CLOSE\' button ')
     // The validation pop-up dialog is opened with the following message:
@@ -97,40 +93,23 @@ describe('Settings > Personnel settings', () => {
      cy.get('.k-grid-content').contains(' No items to display')
 
      cy.log('7. Click on \'ADD\' button and enter previously deleted status name, SAVE ')
-     cy.pause()
      cy.get('.svx-button > .telerik-blazor').click()
-     cy.get(':nth-child(1) > .svx-formfield-content > .input-group > .k-textbox').type('Status 2')
+     cy.get(':nth-child(1) > .svx-formfield-content > .input-group > .k-textbox').type('Status 2').wait(2000)
      cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').click().wait(2000)
      //  The status item is successfully created and added to the grid list.
      cy.get(' .k-link > .svx-font-2').eq(3).click()
      cy.get(' .k-link > .svx-font-2').eq(4).click()
-     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('Status 2')
+     cy.wait(1000)
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Status 2')
      cy.get('.k-grid-content').contains('Status 2')
 
      cy.log('CLEANUP')
-     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').wait(2000).clear().type('Status 1')
+     //  cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('Status 2')
+     cy.get('.k-master-row > [data-col-index="0"]').click()
+     cy.get('.k-button-solid-primary').click()
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('Status 1')
      cy.get('.k-master-row > [data-col-index="0"] > .k-button > .telerik-blazor').click()
      cy.get('.k-button-solid-primary').click()
 
-     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('Status 2')
-     cy.get('.k-master-row > [data-col-index="0"] > .k-button > .telerik-blazor').click()
-     cy.get('.k-button-solid-primary').click()
-
-
-
-
-
-
-
-     
-
-
-
-
-
-  
-
-
-    })
-    
+    })   
 })
