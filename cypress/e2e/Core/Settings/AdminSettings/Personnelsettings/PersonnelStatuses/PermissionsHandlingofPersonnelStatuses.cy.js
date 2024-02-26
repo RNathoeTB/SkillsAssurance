10c285049ea0cd4e5a4467d0d659c3cb11570d7d
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-02-26 12:42:28 +0100

    not finished

--- cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/PersonnelStatuses/PermissionsHandlingofPersonnelStatuses.cy.js
+++ cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/PersonnelStatuses/PermissionsHandlingofPersonnelStatuses.cy.js
@@ -83,7 +83,6 @@ cy.log('\'NO\'/\'YES\' buttons available. BUG >>>>>>>>>> CANCEL & OK is availabl
      cy.log('6. Go to Settings -> Security groups -> Admin security group-> Permissions tab -> Core sub-tab')
      cy.get('#tree-item-12 > .k-link').click()
      cy.get(':nth-child(1) > .svx-settings-container-body > :nth-child(2) > a > div').click()
-     
      cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Admin settings').wait(2000)
      cy.get('[data-render-row-index="2"] > [data-col-index="1"]').contains('Admin settings').dblclick()
      cy.wait(2000)
@@ -104,12 +103,59 @@ cy.log('\'NO\'/\'YES\' buttons available. BUG >>>>>>>>>> CANCEL & OK is availabl
      // 'Save successful' notification message is shown.
 cy.log('BUG, "An unhandled error has occurred. Reload" message is shown')
       
-     cy.log('8. Re-login and navigate to the Personnel statuses overview')
-     cy.get('.profile-picture').click()
-     cy.contains('logout').click()
+    //  cy.log('8. Re-login and navigate to the Personnel statuses overview')
+    //  cy.get('.profile-picture').click()
+    //  cy.contains('logout').click()
+    //  cy.get('#Username').type('Richard')
+    //  cy.get('#Password').type('Nathoe')
+    //  cy.get('#Login').click()
+     cy.get('#tree-item-12 > .k-link > .k-item-text').click()
+     cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
+     cy.get('.k-tabstrip-items').contains('Personnel statuses').click()
      //  User is not able to remove grid records.
      //  Bin icons are not available. 'ADD' button is available.
      cy.log('BUG, records can be deleted, Bin icons are available ')
+
+     cy.log('9. Click \'ADD\', fill all required fields and click \'SAVE\'')
+     cy.get('.svx-button > .telerik-blazor').click()
+     cy.get(':nth-child(1) > .svx-formfield-content > .input-group > .k-textbox').type('TC1284RNTEST')
+     cy.wait(1000)
+     cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').click()
+     cy.get('.svx-modal').should('not.exist')
+     cy.get('.k-link > .svx-font-2').eq(4).should('contain', 'Personnel statuses')
+     //  The user is redirected to the 'Personnel statuses' overview screen
+     cy.get('#tree-item-12 > .k-link > .k-item-text').click()
+     cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
+     cy.get('.k-tabstrip-items').contains('Personnel statuses').click()
+     //  The grid list is updated with newly created item.
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper').type('TC1284RNTEST')
+     cy.get('.k-grid-content').should('contain', 'TC1284RNTEST')
+
+      
+     cy.get('.k-master-row > [data-col-index="1"]').dblclick()
+     cy.get(':nth-child(1) > .svx-formfield-content > .input-group > .k-textbox').type('TC1284RNTESTchange')
+     cy.wait(1000)
+     cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').click()
+     //  The user is redirected to the 'Personnel statuses' overview screen
+     cy.get('#tree-item-12 > .k-link > .k-item-text').click()
+     cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
+     cy.get('.k-tabstrip-items').contains('Personnel statuses').click()
+     //  The grid list is updated with newly created item.
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper').type('TC1284RNTESTchange')
+     cy.get('.k-grid-content').should('contain', 'TC1284RNTESTchange')
+     
+     cy.log('10. Go to Settings -> Security groups -> Admin security group-> Permissions tab -> Core subtab and for Personnel statuses unselect \'Create\' and click \'SAVE\'')
+     cy.get('#tree-item-12 > .k-link').click()
+     cy.get(':nth-child(1) > .svx-settings-container-body > :nth-child(2) > a > div').click()
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Admin settings').wait(2000)
+     cy.get('[data-render-row-index="2"] > [data-col-index="1"]').contains('Admin settings').dblclick()
+     cy.wait(2000)
+     cy.get('.k-link > .svx-font-2').eq(1).click()
+     cy.get('.k-link > .svx-font-2').eq(5).click()
+     
+
+
+
 
 
 
