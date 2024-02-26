c82e825c38e1388a86f6fc89558d9abef810c04e
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-02-26 16:50:49 +0100

    almost finished, some bugs in appl.

--- cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/PersonnelStatuses/PermissionsHandlingofPersonnelStatuses.cy.js
+++ cypress/e2e/Core/Settings/AdminSettings/Personnelsettings/PersonnelStatuses/PermissionsHandlingofPersonnelStatuses.cy.js
@@ -7,6 +7,23 @@ describe('Settings > Personnel settings', () => {
      cy.get('#Password').type('Test123')
      cy.get('#Login').click()
      // Test data:'View'/'Create'/'Update'/'Delete' permissions to 'Personnel statuses' are applied.
+     cy.get('#tree-item-12 > .k-link').click()
+     cy.get(':nth-child(1) > .svx-settings-container-body > :nth-child(2) > a > div').click()
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Admin settings').wait(3000)
+     cy.get('[data-render-row-index="2"] > [data-col-index="1"]').contains('Admin settings').dblclick()
+     cy.wait(2000)
+     cy.get('.k-link > .svx-font-2').eq(1).click()
+     cy.get('.k-link > .svx-font-2').eq(5).click()
+     cy.get(':nth-child(3) > :nth-child(2) > :nth-child(4)').contains('Personnel')
+     cy.get(':nth-child(4) > .svx-permission-block-body > :nth-child(7) > .right-column > .k-button-group > .k-group-end')
+     .invoke('attr', 'aria-pressed').then(ariaPressed => {
+     if (ariaPressed === 'false') {
+      cy.get(':nth-child(4) > .svx-permission-block-body > :nth-child(7) > .right-column > .k-button-group > .k-group-end').click();
+    } else {
+      // If aria-pressed is already false, no action is needed
+    } 
+  }) 
+   cy.get('.modal-buttons > :nth-child(2) > .telerik-blazor').click()
      });
 
     it('Permissions Handling of Personnel Statuses', () => {
@@ -47,7 +64,7 @@ describe('Settings > Personnel settings', () => {
      //  cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('TC1284Permissions')
      //  cy.wait(2000)
      //  cy.get('.k-grid-content').contains('TC1284Permissions')
-cy.log('The grid list is updated with newly created item. >>>>>>>>>>Not updating. BUG')
+cy.log('BUG: The grid list is updated with newly created item. >>Not updating. ')
      cy.get(' .k-link > .svx-font-2').eq(3).click()
      cy.get(' .k-link > .svx-font-2').eq(4).click() 
      cy.wait(1000)
@@ -60,7 +77,7 @@ cy.log('The grid list is updated with newly created item. >>>>>>>>>>Not updating
      cy.wait(2000)
      cy.get('.svx-modal-buttons > :nth-child(2) > .telerik-blazor').click()
      //  The user is redirected to the 'Personnel statuses' overview screen.
-cy.log('The grid list is updated with newly created item.  BUG >>>>>>>>>> Not updating.')
+cy.log('BUG: The grid list is updated with newly created item. > Not updating.')
      cy.get(' .k-link > .svx-font-2').eq(3).click()
      cy.get(' .k-link > .svx-font-2').eq(4).click()
      cy.wait(1000)
@@ -73,7 +90,7 @@ cy.log('The grid list is updated with newly created item.  BUG >>>>>>>>>> Not up
      cy.get('[data-render-row-index="2"] > [data-col-index="0"] > .k-button > .telerik-blazor').click()
      // The pop-up shows with 'Are you sure you want to delete the selected item?
      cy.contains(' Are you sure you want to delete the selected item?').should('exist')
-cy.log('\'NO\'/\'YES\' buttons available. BUG >>>>>>>>>> CANCEL & OK is available ')
+cy.log('BUG: \'NO\'/\'YES\' buttons available. > CANCEL & OK is available ')
 
      cy.log('5. Click \'YES\'')
      cy.get('.k-button-solid-primary').click()
@@ -104,10 +121,11 @@ cy.log('\'NO\'/\'YES\' buttons available. BUG >>>>>>>>>> CANCEL & OK is availabl
       // If aria-pressed is already false, no action is needed
     }
   })
+     cy.get('#blazor-error-ui').click()
      cy.get('.modal-buttons > :nth-child(2) > .telerik-blazor').click()
      // The edit security group dialog is closed.
      // 'Save successful' notification message is shown.
-cy.log('BUG, "An unhandled error has occurred. Reload" message is shown')
+cy.log('BUG: "An unhandled error has occurred. Reload" message is shown')
       
      cy.log('8. Re-login and navigate to the Personnel statuses overview')
      cy.get('.profile-picture').click()
@@ -120,7 +138,7 @@ cy.log('BUG, "An unhandled error has occurred. Reload" message is shown')
      cy.get('.k-tabstrip-items').contains('Personnel statuses').click()
      //  User is not able to remove grid records.
      //  Bin icons are not available. 'ADD' button is available.
-cy.log('ADD and Bin icons are available ')
+cy.log('BUG: ADD and Bin icons are available ')
 
      cy.log('9. Click \'ADD\', fill all required fields and click \'SAVE\'')
      cy.get('.svx-button > .telerik-blazor').click()
@@ -199,11 +217,78 @@ cy.log('BUG, ADD and Bin icons are available ')
      cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper').type('TC1284RNTESTchangestep13')
      cy.get('.k-grid-content').should('contain', 'TC1284RNTESTchangestep13')
 
+     cy.log('14. Go to Settings -> Security groups -> Admin security group-> Permissions tab -> Core subtab and for Personnel statuses unselect \'Update\' and click \'SAVE\'')
+     cy.get('#tree-item-12 > .k-link').click()
+     cy.get(':nth-child(1) > .svx-settings-container-body > :nth-child(2) > a > div').click()
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Admin settings').wait(3000)
+     cy.get('[data-render-row-index="2"] > [data-col-index="1"]').contains('Admin settings').dblclick()
+     cy.wait(2000)
+     cy.get('.k-link > .svx-font-2').eq(1).click()
+     cy.get('.k-link > .svx-font-2').eq(5).click()
+     cy.get(':nth-child(4) > .svx-permission-block-body > :nth-child(7) > .right-column').contains('.telerik-blazor', 'Update')
+     .invoke('attr', 'aria-pressed').then(ariaPressed => {
+     if (ariaPressed === 'true') {
+     cy.get(':nth-child(4) > .svx-permission-block-body > :nth-child(7) > .right-column').contains('.telerik-blazor', 'Update').click();
+    } else {
+      // If aria-pressed is already false, no action is needed
+    }
+  })
+     cy.get('.modal-buttons > :nth-child(2) > .telerik-blazor').click()
+     // The edit security group dialog is closed.
+     // 'Save successful' notification message is shown.
+cy.log('BUG, "An unhandled error has occurred. Reload" message is shown')
 
+     cy.log('15. Re-login and navigate to the Personnel statuses overview')
+     cy.get('.profile-picture').click()
+     cy.contains('Logout').click()
+     cy.get('#Username').type('Richard')
+     cy.get('#Password').type('Test123')
+     cy.get('#Login').click()
+     cy.get('#tree-item-12 > .k-link > .k-item-text').click()
+     cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
+     cy.get('.k-tabstrip-items').contains('Personnel statuses').click()
+     // 'ADD' button is not available. Bin icons are not available.
+cy.log('BUG, ADD and Bin icons are available ')
 
+     cy.log('16. Double-click on any record and observe')
+     // All fields are uneditable.
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').clear().type('TC1284RNTESTchange')
+     cy.wait(2000)
+     cy.get('.k-master-row > [data-col-index="1"]').dblclick().wait(1000)
+     cy.log('BUG, All fields are editable ')
 
+     cy.log('17. Go to Settings -> Security groups -> Admin security group-> Permissions tab -> Core subtab and for Personnel statuses unselect \'View\' and click \'SAVE\'')
+     cy.get('#tree-item-12 > .k-link').click()
+     cy.get(':nth-child(1) > .svx-settings-container-body > :nth-child(2) > a > div').click()
+     cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Admin settings').wait(3000)
+     cy.get('[data-render-row-index="2"] > [data-col-index="1"]').contains('Admin settings').dblclick()
+     cy.wait(2000)
+     cy.get('.k-link > .svx-font-2').eq(1).click()
+     cy.get('.k-link > .svx-font-2').eq(5).click()
+     cy.get(':nth-child(4) > .svx-permission-block-body > :nth-child(7) > .right-column').contains('.telerik-blazor', 'View')
+     .invoke('attr', 'aria-pressed').then(ariaPressed => {
+     if (ariaPressed === 'true') {
+     cy.get(':nth-child(4) > .svx-permission-block-body > :nth-child(7) > .right-column').contains('.telerik-blazor', 'View').click();
+    } else {
+      // If aria-pressed is already false, no action is needed
+    }
+  })
+     cy.get('.modal-buttons > :nth-child(2) > .telerik-blazor').click()
+     // The edit security group dialog is closed.
+     // 'Save successful' notification message is shown.
+cy.log('BUG, "An unhandled error has occurred. Reload" message is shown')
 
-
+     cy.log('18. Re-login and navigate to Settings -> Admin settings ->Personnel settings')
+     cy.get('.profile-picture').click()
+     cy.contains('Logout').click()
+     cy.get('#Username').type('Richard')
+     cy.get('#Password').type('Test123')
+     cy.get('#Login').click()
+     cy.get('#tree-item-12 > .k-link > .k-item-text').click()
+     cy.get(':nth-child(3) > .svx-settings-container-body > :nth-child(3) > a').contains('Personnel settings').click()
+    //  'Personnel statuses' on tabs strip menu is no longer available.
+cy.log('BUG: Personnel statuses on tabs strip menu is STILL available')
+    //  cy.get('.k-tabstrip-items').should('not.contain', 'Personnel statuses')
 
     })   
 })
