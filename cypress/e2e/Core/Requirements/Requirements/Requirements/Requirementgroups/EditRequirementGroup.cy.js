411b7c13ead42c9043226507ea1b95ca5b95c902
Ritchie Nathoe <ritchie.nathoe@testbotics.nl>
2024-03-05 18:02:41 +0100

    untill step 9

--- cypress/e2e/Core/Requirements/Requirements/Requirements/Requirementgroups/EditRequirementGroup.cy.js
+++ cypress/e2e/Core/Requirements/Requirements/Requirements/Requirementgroups/EditRequirementGroup.cy.js
@@ -116,18 +116,18 @@ cy.log('8. Navigate to Personnel -> Employees and open an Employee assigned to a
      cy.get('.k-master-row > [data-col-index="1"]').contains('Cert req group 1-3')
 
 cy.log('9. Navigate to MANAGE -> Requirements -> Requirement groups; open \'Requirement group 1\', remove the OU group added in step 5.')
-     cy.get('#tree-item-8 > .k-link')
-     cy.get('#tree-item-8_1 > .k-link')
+     cy.get('#tree-item-8 > .k-link').click()
+     cy.get('#tree-item-8_1 > .k-link').click()
      cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Requirement group 1 changed').wait(3000)
      cy.get('.k-master-row > [data-col-index="1"]').contains('Requirement group 1').dblclick().wait(3000)
-     cy.get(':nth-child(4) > .svx-formfield-content > .telerik-blazor').find('.k-input-inner').clear()
+     cy.get('.k-multiselect').find('.k-input-inner').type('{backspace}')
      cy.get('.modal-buttons > :nth-child(2) > .telerik-blazor').click()
      //  The user is redirected to the Requirement groups overview screen.
      cy.get('.svx-page-header').contains('Requirement groups')
      //  The change has been successfully saved and shows updated in the grid.
      cy.get('[data-col-index="1"] > .k-filtercell > .k-filtercell-wrapper > .k-textbox').type('Requirement group 1 changed').wait(3000)
-     cy.get('.k-alt > [data-col-index="4"]').should('be.empty')
-     
+     cy.get('.k-alt > [data-col-index="4"]').should('not.contain', 'Americas')   
+
 
 
 
